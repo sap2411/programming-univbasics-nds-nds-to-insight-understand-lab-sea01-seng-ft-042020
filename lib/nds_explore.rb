0ebc352622a8aa4cbce117e950261647f2d84b84
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-21 04:01:00 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ end
 
 def print_first_directors_movie_titles
   n = 0
-  while n < directors_database[0][:movie].count do
+  while n < directors_database[0][:movie] do
  puts directors_database[0][:movies][n][:title]
  n += 1
  end
